62f2ffb614cda2362fd5f1f62ebd5aa219edfa80
Mark Spirek <MSpirek@gmail.com>
2012-11-07 05:43:35 -0600

    Settings wording

--- Notepad RT/Notepad RT/js/settings.js
+++ Notepad RT/Notepad RT/js/settings.js
@@ -7,7 +7,7 @@ function initializeSettings() {
     // Populate settings pane and tie commands to settings flyouts.
     WinJS.Application.onsettings = function (e) {
         e.detail.applicationcommands = {
-            "defaultsDiv": { href: "/html/DefaultsUI.html", title: "Editor Defaults" },
+            "defaultsDiv": { href: "/html/DefaultsUI.html", title: "Editor" },
             "aboutDiv": { href: "/html/SupportUI.html", title: "Feedback and Support" }
         };
         WinJS.UI.SettingsFlyout.populateSettings(e);
