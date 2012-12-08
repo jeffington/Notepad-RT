3dea5f0c701d01d5b9dd1434ab9051ea993351e7
Jeff Meyerhoff <jeff@jdmlabs.org>
2012-12-08 17:34:53 -0600

    Made the recent files list wonderful

--- Notepad RT/Notepad RT/js/recentFilesList.js
+++ Notepad RT/Notepad RT/js/recentFilesList.js
@@ -107,9 +107,9 @@ function loadThumbnails() {
 
 }
 
-function initData() {
+/*function initData() {
 
-    var sessionFileList = WinJS.Application.sessionState.files;
+    //var sessionFileList = WinJS.Application.sessionState.files;
     // TODO: Need some logic here to determine whether or not the data changed
 
     if (sessionFileList && sessionFileList.length > 0) {
@@ -117,7 +117,7 @@ function initData() {
         // Set up the listView from the session data
         console.log("We have the file info.");
 
-        recentFilesDataSource = new WinJS.Binding.List(sessionFileList);
+        recentFilesDataSource = new desertsDataAdapter();//new WinJS.Binding.List(sessionFileList);
 
         recentFilesListView = new WinJS.UI.ListView(document.getElementById("filesListView"), {
             itemDataSource: recentFilesDataSource.dataSource,//recentFilesDataSource.dataSource,
@@ -143,26 +143,35 @@ function initData() {
 
     }
 
-}
-/*
-// Sorts the groups by first letter
-function compareGroups(left, right) {
-    return 0;//left.toUpperCase().charCodeAt(0) - right.toUpperCase().charCodeAt(0);
-}
+} /* This is the initData() method using the old and shitty List.Bind */
 
-// Gets the key of the group that an item belongs to
-function getGroupKey(dataItem) {
-    return dataItem.key;
-}
+function initData() {
 
-// Gets the data for a group
-function getGroupData(dataItem) {
-    return {
-        groupTitle: "Recent",
-        groupDetails: "7 items",
-    };
+    recentFilesDataSource = new desertsDataSource();
+
+    /*
+    recentFilesListView = new WinJS.UI.ListView(document.getElementById("filesListView"), {
+        itemDataSource: recentFilesDataSource,//.dataSource,//recentFilesDataSource.dataSource,
+        //groupDataSource: groupDataSource.groups.dataSource,//groupDataSource,//.groupDataSource,//DataSource.dataSource,//groupDataSource.groups.dataSource,
+        itemTemplate: document.getElementById("imageTextListFileTemplate"),
+        groupHeaderTemplate: document.getElementById("groupHeaderTemplate"),
+        layout: new WinJS.UI.GridLayout(),
+        selectionMode: WinJS.UI.SelectionMode.single,
+        oniteminvoked: recentFilesSelection,
+        tapBehavior: WinJS.UI.TapBehavior.invokeOnly,
+        swipeBehavior: 'none',
+    });
+    /*  */
+
+    recentFilesListView = new WinJS.UI.ListView(document.getElementById("filesListView"), {
+        itemDataSource: recentFilesDataSource,
+        //groupDataSource: groupDataSource,
+        itemTemplate: document.getElementById("imageTextListFileTemplate"),
+        //groupHeaderTemplate: document.getElementById("groupHeaderTemplate"),
+        layout: new WinJS.UI.GridLayout()
+    });
+
 }
-/* This is left over from a previous attempt to create the group headers */
 
 function recentFilesSelection(event) {
 
@@ -179,114 +188,116 @@ function recentFilesSelection(event) {
     WinJS.Navigation.navigate("/pages/editor/editorPage.html", { filetoken: selectedFileToken, filename: selectedFileName });
 
 }
-/*
-var desertsDataAdapter = WinJS.Class.define(
-    function (groupData) {
-        // Constructor
-        this._groupData = groupData;
-    },
 
-    // Data Adapter interface methods
-    // These define the contract between the virtualized datasource and the data adapter.
-    // These methods will be called by virtualized datasource to fetch items, count etc.
+var recentFilesDataAdapter = WinJS.Class.define(
+    function () { //groupData) {
+        // Constructor
+        //this._groupData = groupData;
+    },
     {
         // This example only implements the itemsFromIndex, itemsFromKey and count methods
 
         // Called to get a count of the items, this can be async so return a promise for the count
         getCount: function () {
-            var that = this;
-            return WinJS.Promise.wrap(that._groupData.length);
-        },
+            //var that = this;
 
-        // Called by the virtualized datasource to fetch a list of the groups based on group index
-        // It will request a specific group and hints for a number of groups either side of it
-        // The implementation should return the specific group, and can choose how many either side
-        // to also send back. It can be more or less than those requested.
-        //
+            return WinJS.Promise.wrap(Windows.Storage.AccessCache.StorageApplicationPermissions.mostRecentlyUsedList.entries.size);
+            //that._groupData.length);
+
+        },
         // Must return back an object containing fields:
         //   items: The array of groups of the form:
         //      [{ key: groupkey1, firstItemIndexHint: 0, data : { field1: value, field2: value, ... }}, { key: groupkey2, firstItemIndexHint: 27, data : {...}}, ...
         //   offset: The offset into the array for the requested group
         //   totalCount: (optional) an update of the count of items
+        //var lastFetchIndex = Math.min(requestIndex + countAfter, that._groupData.length - 1);
+        //var fetchIndex = Math.max(requestIndex - countBefore, 0);
+        //var results = [];
         itemsFromIndex: function (requestIndex, countBefore, countAfter) {
+
             var that = this;
 
-            if (requestIndex >= that._groupData.length) {
-                return Promise.wrapError(new WinJS.ErrorFromName(UI.FetchError.doesNotExist));
-            }
-
-            var lastFetchIndex = Math.min(requestIndex + countAfter, that._groupData.length - 1);
             var fetchIndex = Math.max(requestIndex - countBefore, 0);
-            var results = [];
 
-            // form the array of groups
-            for (var i = fetchIndex; i <= lastFetchIndex; i++) {
-                var group = that._groupData[i];
-                results.push({
-                    key: group.key,
-                    firstItemIndexHint: group.firstItemIndex,
-                    data: group
-                });
-            }
-            return WinJS.Promise.wrap({
-                items: results, // The array of items
-                offset: requestIndex - fetchIndex, // The offset into the array for the requested item
-                totalCount: that._groupData.length // The total count
-            });
-        },
+            console.log("Fetching: " + fetchIndex + " " + that.getCount());
 
-        // Called by the virtualized datasource to fetch groups based on the group's key
-        // It will request a specific group and hints for a number of groups either side of it
-        // The implementation should return the specific group, and can choose how many either side
-        // to also send back. It can be more or less than those requested.
-        //
-        // Must return back an object containing fields:
-        //   [{ key: groupkey1, firstItemIndexHint: 0, data : { field1: value, field2: value, ... }}, { key: groupkey2, firstItemIndexHint: 27, data : {...}}, ...
-        //   offset: The offset into the array for the requested group
-        //   absoluteIndex: the index into the list of groups of the requested group
-        //   totalCount: (optional) an update of the count of items
-        itemsFromKey: function (requestKey, countBefore, countAfter) {
-            var that = this;
-            var requestIndex = null;
+            if (fetchIndex >= Windows.Storage.AccessCache.StorageApplicationPermissions.mostRecentlyUsedList.entries.size) {
 
-            // Find the group in the collection
-            for (var i = 0, len = that._groupData.length; i < len; i++) {
-                if (that._groupData[i].key === requestKey) {
-                    requestIndex = i;
-                    break;
-                }
-            }
-            if (requestIndex === null) {
                 return WinJS.Promise.wrapError(new WinJS.ErrorFromName(WinJS.UI.FetchError.doesNotExist));
+
             }
 
-            var lastFetchIndex = Math.min(requestIndex + countAfter, that._groupData.length - 1);
-            var fetchIndex = Math.max(requestIndex - countBefore, 0);
-            var results = [];
+            
 
-            //iterate and form the collection of the results
-            for (var j = fetchIndex; j <= lastFetchIndex; j++) {
-                var group = that._groupData[j];
-                results.push({
-                    key: group.key, // The key for the group
-                    firstItemIndexHint: group.firstItemIndex, // The index into the items for the first item in the group
-                    data: group // The data for the specific group
-                });
-            }
+            
+            var token = Windows.Storage.AccessCache.StorageApplicationPermissions.mostRecentlyUsedList.entries.getAt(fetchIndex).token;
+            
 
-            // Results can be async so the result is supplied as a promise
-            return WinJS.Promise.wrap({
-                items: results, // The array of items
-                offset: requestIndex - fetchIndex, // The offset into the array for the requested item
-                absoluteIndex: requestIndex, // The index into the collection of the item referenced by key
-                totalCount: that._groupData.length // The total length of the collection
+            
+
+            var currentObject;
+
+            return Windows.Storage.AccessCache.StorageApplicationPermissions.mostRecentlyUsedList.getFileAsync(token).then(function (currentFile) {
+
+                currentObject = {
+                    items: [
+                        {
+                            key: requestIndex.toString(),
+                            //firstItemIndexHint: 0,
+                            //groupKey: 'R',
+                            data: {
+                                icon: "images/filelogo.png",
+                                title: currentFile.name,
+                                textType: currentFile.displayType,
+                                size: "",
+                                // sourceIcon: "",
+                                //kind: "R",
+                                token: token,
+                            }
+
+                        }
+                    ],
+                    offset: requestIndex - fetchIndex,
+                    
+                }
+
+                return currentFile.getThumbnailAsync(Windows.Storage.FileProperties.ThumbnailMode.documentsView);
+                
+
+                
+            }).then(function(thumb) {
+
+                currentObject.items[0].data.icon = URL.createObjectURL(thumb, { oneTimeOnly: false });
+
+                return WinJS.Promise.wrap(currentObject);
+
             });
+            //currentFile.getThumbnailAsync(Windows.Storage.FileProperties.ThumbnailMode.documentsView).then(function (thumb) {
+            //});
+            /* */
+            /*return WinJS.Promise.wrap({
+                items: {
+                    key: 'R', // The key for the group
+                    firstItemIndexHint: 0, // The index into the items for the first item in the group
+                    data: {
+                        icon: "images/filelogo.png",
+                        title: "TEST",
+                        textType: "TEST TYPE",
+                        size: "",
+                        // sourceIcon: "",
+                        kind: "R",
+                        //token: token,
+                    } // The data for the specific group
+                }, // The array of items
+                offset: requestIndex, // The offset into the array for the requested item
+                absoluteIndex: requestIndex, // The index into the collection of the item referenced by key
+                totalCount: that.getCount() // The total length of the collection
+            });*/
         },
-
     });
 
 // Create a DataSource by deriving and wrapping the data adapter with a VirtualizedDataSource
-var desertsDataSource = WinJS.Class.derive(WinJS.UI.VirtualizedDataSource, function (data) {
-    this._baseDataSourceConstructor(new desertsDataAdapter(data));
+var desertsDataSource = WinJS.Class.derive(WinJS.UI.VirtualizedDataSource, function () {//data) {
+    this._baseDataSourceConstructor(new recentFilesDataAdapter());//data));
 });
 /* This is left over. */
